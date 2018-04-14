1de9101c1f64fee2daa87f22fecb594dd93e59bf
UnshackledJester <36856266+UnshackledJester@users.noreply.github.com>
2018-04-13 20:27:47 -0400

    Structure update

---
+++ Project340/src/Interface/ServerInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+import Models.Customer;
+/**
+ *
+ * @author Chronos
+ */
+public class ServerInterface {
+    public Customer searchAccount(String id, String name){
+        Customer ted;
+        ted = new Customer();
+        
+        
+        return ted;
+    }
+}
