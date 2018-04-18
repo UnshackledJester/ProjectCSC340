9326eaea16a7d089d1dd5b41e16314fa1d78d5fc
hvvo <38018429+hvvo@users.noreply.github.com>
2018-04-18 18:08:47 -0400

    fix

--- IRMA/src/Models/Authentication.java
+++ IRMA/src/Models/Authentication.java
@@ -1,18 +1,17 @@
 package Models;
 
-
 import static Interfaces.DatabaseInterface.getEmployee;
 import static Interfaces.DatabaseInterface.getManager;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Authentication {
 
 //Test if manager Username and Password combination exists.
-
     public static boolean testForManager(String pass, String user) throws IOException, Exception {
-        Manager[] array = getManager();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].getUsername().equals(user) && array[i].getPassword().equals(pass)) {
+        ArrayList<Manager> arr = new ArrayList<Manager>();
+        for (Manager manager : arr) {
+            if (manager.getUsername().equals(user) && manager.getPassword().equals(pass)) {
                 return true;
             }
         }
@@ -20,16 +19,6 @@ public class Authentication {
     }
     //Test if Employee Username and Password combination exists.
 
-    public static boolean testForEmployees(String pass, String user) throws IOException, Exception {
-        Employee[] array = getEmployee();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].getUsername().equals(user) && array[i].getPassword().equals(pass)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     public static boolean testForAdmin(String pass, String user) {
         return (user.equals("Admin") && pass.equals("Password"));
