283448b151bfbdcbc6fd71c9bedabe27e373d0a2
hvvo <38018429+hvvo@users.noreply.github.com>
2018-04-27 14:58:59 -0400

    Fix hash

--- IRMA/src/Utility/HashPassword.java
+++ IRMA/src/Utility/HashPassword.java
@@ -1,44 +1,63 @@
 package Utility;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class HashPassword {
-  //  String str; 
-   static  Map<Character,Character> map1=new HashMap<Character,Character>();
-   static  Map<Character,Character> map2=new HashMap<Character,Character>();
-   HashPassword(){
-       //hash map
-        String PasswordChecking="abcdefghijklmnopqrstuvwsxyz*0123456789QWERTYUIOPASDFGHJKLZXCVBNM_";
-          String TranslateTable="9876543210*abcdefghijklmnopqrstuvwsxyzMNBVCXZ_LKJHGFDSAPOIUYTREWQ";
+    //  String str; 
+
+    static Map<Character, Character> map1 = new HashMap<Character, Character>();
+    static Map<Character, Character> map2 = new HashMap<Character, Character>();
+
+    HashPassword() {
+        //hash map
+        String PasswordChecking = "abcdefghijklmnopqrstuvwsxyz*0123456789QWERTYUIOPASDFGHJKLZXCVBNM_";
+        String TranslateTable = "9876543210*abcdefghijklmnopqrstuvwsxyzMNBVCXZ_LKJHGFDSAPOIUYTREWQ";
         //9876543210*abcdefghijklmnopqrstuvwsxyzMNBVCXZ_LKJHGFDSAPOIUYTREWQ
-        for(int i=0;i<PasswordChecking.length();i++){
-            map1.put(PasswordChecking.charAt(i),TranslateTable.charAt(i));
-            map2.put(TranslateTable.charAt(i),PasswordChecking.charAt(i));
+        for (int i = 0; i < PasswordChecking.length(); i++) {
+            map1.put(PasswordChecking.charAt(i), TranslateTable.charAt(i));
+            map2.put(TranslateTable.charAt(i), PasswordChecking.charAt(i));
         }
     }
-   // Hashing a password
-   public  static String encryption(String Password){
-        String password= Password;
-       String HashedPassword = "";
-       for (int i = 0; i < password.length(); i++) {
-           HashedPassword = HashedPassword + map1.get(password.charAt(i));
-       }
-       return HashedPassword;
+    // Hashing a password
+
+    public static String encryption(String Password) {
+        String password = Password;
+        String HashedPassword = "";
+        for (int i = 0; i < password.length(); i++) {
+            HashedPassword = HashedPassword + map1.get(password.charAt(i));
+        }
+        return HashedPassword;
     }
-   // get the password back from hashing
-    public static String decryption(String Password){
+    // get the password back from hashing
+
+    public static String decryption(String Password) {
         String NormalPassword = "";
         for (int i = 0; i < Password.length(); i++) {
             NormalPassword = NormalPassword + map2.get(Password.charAt(i));
         }
         return NormalPassword;
     }
-    public static class String Hashed(String hashed){
+
+    public static String Hashed(String hashed) {
         HashPassword HP = new HashPassword();
         String str = hashed;
-        String str1 = HP.encryption(str);
+        String str1 = HashPassword.encryption(str);
         return str1;
-        
-        
+
+    }
+
+    public static String deHashed(String dehashed) {
+        HashPassword HP = new HashPassword();
+        String str = dehashed;
+        String str1 = HashPassword.decryption(str);
+        return str1;
+
+    }
+
+    public static void main(String[] args) {
+        String Str1 = "Password";
+        System.out.println(Hashed(Str1));
+        System.out.println(deHashed(Hashed(Str1)));
     }
 }
