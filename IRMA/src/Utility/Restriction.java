9c8c87ac6725cc591223a77cbb7b07bc65a446e2
UnshackledJester <36856266+UnshackledJester@users.noreply.github.com>
2018-04-27 18:53:09 -0400

    Last push

--- IRMA/src/Utility/Restriction.java
+++ IRMA/src/Utility/Restriction.java
@@ -1,27 +1,35 @@
 package Utility;
+//Restriction on username and password.
 
 public class Restriction {
-public static boolean hasLength(CharSequence data) {
-    if (String.valueOf(data).length() >= 8) 
-        return true;
-    else return false;
-}
+//check for the length.
 
-public static boolean hasSymbol(CharSequence data) {
-    String password = String.valueOf(data);
-    boolean hasSpecial = !password.matches("[A-Za-z0-9 ]*");
-    return hasSpecial;
-}
+    public static boolean hasLength(CharSequence _data) {
+        if (String.valueOf(_data).length() >= 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+//check for symbol.
 
-public static boolean hasUpperCase(CharSequence data) {
-    String password = String.valueOf(data);
-    boolean hasUppercase = !password.equals(password.toLowerCase());
-    return hasUppercase;
-}
+    public static boolean hasSymbol(CharSequence _data) {
+        String password = String.valueOf(_data);
+        boolean hasSpecial = !password.matches("[A-Za-z0-9 ]*");
+        return hasSpecial;
+    }
+//check for uppercase.
 
-public static boolean hasLowerCase(CharSequence data) {
-    String password = String.valueOf(data);
-    boolean hasLowercase = !password.equals(password.toUpperCase());
-    return hasLowercase;
-}
+    public static boolean hasUpperCase(CharSequence _data) {
+        String password = String.valueOf(_data);
+        boolean hasUppercase = !password.equals(password.toLowerCase());
+        return hasUppercase;
+    }
+//check for lowercase.
+
+    public static boolean hasLowerCase(CharSequence _data) {
+        String password = String.valueOf(_data);
+        boolean hasLowercase = !password.equals(password.toUpperCase());
+        return hasLowercase;
+    }
 }
